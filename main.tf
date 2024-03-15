86b16621c2eddd7495f8ba3c2060a403097c3e62
EdgarSargsyan <sargsyanedg@gmail.com>
2024-03-15 13:15:00 +0000

    terraform homework

---
+++ main.tf
@@ -0,0 +1,86 @@
+# general purpose stuff
+data "aws_ssm_parameter" "my_amzn_linux_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+
+locals {
+	ec2_name_prefix = "esargsyan_ec2_tf_"
+}
+
+
+# security group stuff
+data "aws_security_group" "sg" {
+  name = var.security_group_name
+}
+
+resource "aws_security_group" "sg-tf" {
+  name        = var.security_group_name
+  description = var.security_group_description
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // Allow traffic from any IP address
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          // Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"] // Allow traffic to any IP address
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+
+
+# Launch template stuff
+# !!! user_data makes trubles, ommited for now
+resource "aws_launch_template" "launch_template_tf" {
+  name_prefix = var.launch_template_name
+  description = "EC2 launch template generated from terraform"
+
+  network_interfaces {
+    device_index                = 0
+    associate_public_ip_address = true
+    security_groups             = [data.aws_security_group.sg.id]
+  }
+
+  instance_type = var.instance_type_tf
+  key_name      = var.key_pair_tf
+  image_id      = data.aws_ssm_parameter.my_amzn_linux_ami.insecure_value
+
+
+  placement {
+    availability_zone = var.availability_zone_tf
+  }
+}
+
+
+# EC2 stuff
+resource "aws_instance" "ec2_tf" {
+  count = 3
+
+  launch_template {
+    id      = aws_launch_template.launch_template_tf.id
+    version = "$Latest"
+  }
+
+  tags = {
+    Name = "esargsyan_ec2_tf_${format("%01d", count.index + 1)}"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "security_group_name" {
+  type = string
+}
+
+variable "security_group_description" {
+  type = string
+}
+
+variable "launch_template_name" {
+  type = string
+}
+
+variable "instance_type_tf" {
+  type = string
+}
+
+variable "key_pair_tf" {
+  type = string
+}
+
+variable "availability_zone_tf" {
+  type = string
+}
